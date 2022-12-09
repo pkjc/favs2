ecf471a13af0bd6b268ffda2100e4fb60f907843
Pankaj <mepankaj@live.in>
2022-12-09 18:41:06 +0000

    updates

--- components/UserBioCard/AddFav.tsx
+++ components/UserBioCard/AddFav.tsx
@@ -19,26 +19,31 @@ export function AddFav() {
   }
 
   useEffect(() => {
-    async function getMoviesAndTvByKeyword(searchQuery : string) {
-      const TMDB_SEARCH_URL = `https://api.themoviedb.org/3/search/multi?&api_key=5f4fbd1cb92deca8b31627ac4d56951f&page=1&include_adult=false&query=${searchQuery}`;
-      const resp = await fetch(TMDB_SEARCH_URL);
-      const searchResults = await resp.json();
-      return searchResults;
+    // console.log("1: selected: " + JSON.stringify(selected) + " searchQuery: " + searchQuery);
+    if(!searchQuery) {
+      console.log("2: selected: " + JSON.stringify(selected) + " searchQuery: " + searchQuery);
+      setOptions([]);
+      setSelected({});
     }
-    
     if(JSON.stringify(selected) === '{}' && searchQuery && searchQuery.length > 2) {
-      console.log("Input changed. Calling API");
+      // console.log("Input changed. Calling API");
       getMoviesAndTvByKeyword(searchQuery).then((moviesOrTv) => {
         if (!moviesOrTv) return;
         let ops = moviesOrTv.results.map((item : any) => {
           if(item && (item.media_type === 'movie' || item.media_type === 'tv')){
-            return {...item, value: (item.name || item.title || "Emtpy"), group: groupName[item.media_type as keyof MediaType]};
+            return {
+              ...item, 
+              value: (item.name || item.title || "Emtpy") + " (" + (item.release_date && item.release_date.slice(0, 4)) + ") ", 
+              group: groupName[item.media_type as keyof MediaType]
+            };
           }
         });
         ops = ops.filter( Boolean );
         console.log("ops: ", ops);
         setOptions(ops);
       });
+    } else {
+      // console.log("3: selected: " + JSON.stringify(selected) + " searchQuery: " + searchQuery);
     }
   }, [searchQuery]);
 
@@ -61,14 +66,14 @@ export function AddFav() {
   }
   
   const AutoCompleteItem = forwardRef<HTMLDivElement, ItemProps>(
-    ({ release_date, value, poster_path, vote_average, ...others }: ItemProps, ref) => (
-      <div ref={ref} {...others} key={release_date}>
+    ({ release_date, value, poster_path, vote_average, id, ...others }: ItemProps, ref) => (
+      <div ref={ref} {...others}>
         <Group>
           <Avatar variant="outline" radius="xs" size="lg" src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
           <div>
             <Text>{value}</Text>
             <Text size="xs" color="dimmed">
-              released: {release_date && release_date.slice(0, 4)} / imdb: {vote_average} / 10
+              IMDB: {vote_average} / 10
             </Text>
           </div>
         </Group>
@@ -92,6 +97,9 @@ export function AddFav() {
           dropdownComponent={ScrollArea}
           onItemSubmit={handleItemSelected}
           limit={20}
+          filter={(value, item) =>
+            item.value.toLowerCase().includes(value.toLowerCase().trim())
+          }
         />
       </Grid.Col>
       <Grid.Col span={4}>
@@ -108,7 +116,10 @@ export function AddFav() {
     </Grid>
   );
 }
-function lookUpGroupName(media_type: any) {
-  
-}
 
+async function getMoviesAndTvByKeyword(searchQuery : string) {
+  const TMDB_SEARCH_URL = `https://api.themoviedb.org/3/search/multi?&api_key=5f4fbd1cb92deca8b31627ac4d56951f&page=1&include_adult=false&query=${searchQuery}`;
+  const resp = await fetch(TMDB_SEARCH_URL);
+  const searchResults = await resp.json();
+  return searchResults;
+}
